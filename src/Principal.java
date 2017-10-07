8071c7a02a1c0a694143949ccd198f5823d2b962
Tecnico - TI <tecnico@epfserver.com.br>
2017-10-06 21:58:36 -0300

    Criação do projeto e calculadora com a operação de soma

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 10;
+		int valor2 = 50;
+		
+		JOptionPane.showMessageDialog(null, "O valor da soma é: " + (valor1 + valor2));
+	}
+}
